f5ceaec2fb764a49905cabf9314802d29ca7634a
AlexFrndz <fernandezalcaidealejandro@gmail.com>
2022-04-12 20:32:25 +0200

    Downgrade to React 17 Antd won't support React 18 until v5 is released, we'll upgrade when that rolls out

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
+//import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
 import { Navigation } from "./navigation";
 import store from "./redux";
 
 const container = document.getElementById("root") as HTMLElement;
-const root = createRoot(container);
 
+ReactDOM.render(
+  <Provider store={store}>
+    <Navigation />
+  </Provider>,
+  container
+);
+
+/*
+This is React 18 code. We'll upgrade once again when antd v5 with React 18 support is released
+const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <Navigation />
   </Provider>
 );
+*/
