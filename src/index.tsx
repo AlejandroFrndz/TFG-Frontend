6b2c03239b079f4307d9dd1792c90abfb580c6c1
AlexFrndz <fernandezalcaidealejandro@gmail.com>
2022-04-12 19:45:41 +0200

    Added root reducer and store

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import { Navigation } from "./navigation";
+import store from "./redux";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
-root.render(<Navigation />);
+
+root.render(
+  <Provider store={store}>
+    <Navigation />
+  </Provider>
+);
