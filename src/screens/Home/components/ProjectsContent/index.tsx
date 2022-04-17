228ce0d6262c7c01f34319ef39ea870461307d93
AlexFrndz <fernandezalcaidealejandro@gmail.com>
2022-04-17 14:55:21 +0200

    Added actual folder logic with backend data

--- src/screens/Home/components/ProjectsContent/index.tsx
+++ src/screens/Home/components/ProjectsContent/index.tsx
@@ -1,37 +1,39 @@
 import { Col, Row } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { CSSProperties } from "react";
+import { useSelector } from "react-redux";
+import { selectFolders } from "src/redux/auth/selectors";
 import { Folder } from "src/screens/Home/components/ProjectsContent/components/Folder";
+import { IFolder } from "src/utils/api/resources/folder";
 
 export const ProjectsContent: React.FC = () => {
+  const [activeFolder, setActiveFolder] = useState<null | string>(null);
+  const [folders] = useState<IFolder[]>(useSelector(selectFolders));
+  const [activeFolders, setActiveFolders] = useState<IFolder[]>([]);
+
+  useEffect(() => {
+    setActiveFolders(
+      folders.filter((folder) => folder.parent === activeFolder)
+    );
+  }, [activeFolder, folders]);
+
   return (
-    <Row
-      gutter={[16, 24]}
-      style={{ width: "100%", paddingTop: "4vh", paddingLeft: "1vw" }}
-    >
-      <Col span={6}>
-        <Folder name="folder" id="a" />
-      </Col>
-      <Col span={6}>
-        <Folder name="folder" id="b" />
-      </Col>
-      <Col span={6}>
-        <Folder name="folder" id="c" />
-      </Col>
-      <Col span={6}>
-        <Folder name="folder" id="d" />
-      </Col>
-      <Col span={6}>
-        <Folder name="folder" id="e" />
-      </Col>
-      <Col span={6}>
-        <Folder name="folder" id="f" />
-      </Col>
-      <Col span={6}>
-        <Folder name="folder" id="g" />
-      </Col>
-      <Col span={6}>
-        <Folder name="folder" id="h" />
-      </Col>
+    <Row gutter={[16, 24]} style={styles.mainRow}>
+      {activeFolders.map((folder) => {
+        return (
+          <Col span={6} onDoubleClick={() => setActiveFolder(folder.id)}>
+            <Folder name={folder.name} id={folder.id} />
+          </Col>
+        );
+      })}
     </Row>
   );
+};
+
+const styles = {
+  mainRow: {
+    width: "100%",
+    paddingTop: "4vh",
+    paddingLeft: "1vw",
+  } as CSSProperties,
 };
