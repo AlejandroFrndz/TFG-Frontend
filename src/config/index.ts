9f5d06d90454e89e6d19d855664a63609c457b39
AlexFrndz <fernandezalcaidealejandro@gmail.com>
2022-04-23 18:17:49 +0200

    Added env config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,20 @@
+const isDevEnv = process.env.NODE_ENV === "development";
+const isTestEnv = process.env.NODE_ENV === "test";
+
+const isProdEnv = !isDevEnv && !isTestEnv;
+
+if (!isProdEnv) {
+  const dotenv = require("dotenv");
+  console.log("Loading config from .env");
+  dotenv.config({ path: "src/config/.env" });
+}
+
 const config = {
-  baseUrl: "http://localhost:5000",
+  isDevEnv,
+  isTestEnv,
+  isProdEnv,
+
+  baseUrl: process.env.REACT_APP_API_URL || "http://localhost:5000",
 };
 
 export default config;
