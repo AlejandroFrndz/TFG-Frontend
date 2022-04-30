d3df2ab237e4b0a005e9a5c4584d791a15e4601c
AlexFrndz <fernandezalcaidealejandro@gmail.com>
2022-04-30 19:25:46 +0200

    Removed image from WIP component

--- src/screens/WIP/index.tsx
+++ src/screens/WIP/index.tsx
@@ -1,18 +1,10 @@
-import { Col, Row } from "antd";
 import React, { CSSProperties } from "react";
 import { Center } from "src/shared/Center/Center";
 
 export const WIP: React.FC = () => {
   return (
     <Center style={styles.centerInPage}>
-      <Row justify="center">
-        <Col span={24}>
-          <img src={require("../../assets/under-construction.jpg")} alt="WIP" />
-        </Col>
-        <Col span={24}>
-          <h1>We're working on it 🛠</h1>
-        </Col>
-      </Row>
+      <h1>We're working on it 🛠</h1>
     </Center>
   );
 };
