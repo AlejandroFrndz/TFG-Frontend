df622ecc4f56b160913d3ff87c9664703b25c58b
AlexFrndz <fernandezalcaidealejandro@gmail.com>
2022-05-02 20:20:32 +0200

    FullScreen loader component

---
+++ src/shared/FullScreenLoader/index.tsx
@@ -0,0 +1,147 @@
+import { CSSProperties } from "react";
+import {
+  BarLoader,
+  BeatLoader,
+  BounceLoader,
+  CircleLoader,
+  ClimbingBoxLoader,
+  ClipLoader,
+  ClockLoader,
+  DotLoader,
+  FadeLoader,
+  GridLoader,
+  HashLoader,
+  MoonLoader,
+  PacmanLoader,
+  PropagateLoader,
+  PuffLoader,
+  PulseLoader,
+  RingLoader,
+  RiseLoader,
+  RotateLoader,
+  ScaleLoader,
+  SkewLoader,
+  SquareLoader,
+  SyncLoader,
+} from "react-spinners";
+import { Center } from "../Center/Center";
+
+type LoaderType =
+  | "Bar"
+  | "Beat"
+  | "Bounce"
+  | "Circle"
+  | "ClimbingBox"
+  | "Clip"
+  | "Clock"
+  | "Dot"
+  | "Fade"
+  | "Grid"
+  | "Hash"
+  | "Moon"
+  | "Pacman"
+  | "Propagate"
+  | "Puff"
+  | "Pulse"
+  | "Ring"
+  | "Rise"
+  | "Rotate"
+  | "Scale"
+  | "Skew"
+  | "Square"
+  | "Sync";
+
+type FullScreenLoaderProps = {
+  type: LoaderType;
+  size?: number;
+  height?: number;
+  width?: number;
+  margin?: number;
+  radius?: number;
+  color?: string;
+};
+
+export const FullScreenLoader: React.FC<FullScreenLoaderProps> = ({
+  type,
+  size,
+  height,
+  width,
+  margin,
+  radius,
+  color,
+}) => {
+  const renderLoader = () => {
+    switch (type) {
+      case "Bar":
+        return <BarLoader height={height} width={width} color={color} />;
+      case "Beat":
+        return <BeatLoader size={size} margin={margin} color={color} />;
+      case "Bounce":
+        return <BounceLoader size={size} color={color} />;
+      case "Circle":
+        return <CircleLoader size={size} color={color} />;
+      case "ClimbingBox":
+        return <ClimbingBoxLoader size={size} color={color} />;
+      case "Clip":
+        return <ClipLoader size={size} color={color} />;
+      case "Clock":
+        return <ClockLoader size={size} color={color} />;
+      case "Dot":
+        return <DotLoader size={size} color={color} />;
+      case "Fade":
+        return (
+          <FadeLoader
+            height={height}
+            width={width}
+            radius={radius}
+            margin={margin}
+            color={color}
+          />
+        );
+      case "Grid":
+        return <GridLoader size={size} margin={margin} color={color} />;
+      case "Hash":
+        return <HashLoader size={size} color={color} />;
+      case "Moon":
+        return <MoonLoader size={size} color={color} />;
+      case "Pacman":
+        return <PacmanLoader size={size} margin={margin} color={color} />;
+      case "Propagate":
+        return <PropagateLoader size={size} color={color} />;
+      case "Puff":
+        return <PuffLoader size={size} color={color} />;
+      case "Pulse":
+        return <PulseLoader size={size} margin={margin} color={color} />;
+      case "Ring":
+        return <RingLoader size={size} color={color} />;
+      case "Rise":
+        return <RiseLoader size={size} margin={margin} color={color} />;
+      case "Rotate":
+        return <RotateLoader size={size} margin={margin} color={color} />;
+      case "Scale":
+        return (
+          <ScaleLoader
+            height={height}
+            width={width}
+            radius={radius}
+            margin={margin}
+            color={color}
+          />
+        );
+      case "Skew":
+        return <SkewLoader size={size} color={color} />;
+      case "Square":
+        return <SquareLoader size={size} color={color} />;
+      case "Sync":
+        return <SyncLoader size={size} margin={margin} color={color} />;
+    }
+  };
+  return <Center style={styles.fullScreenSize}>{renderLoader()}</Center>;
+};
+
+const styles = {
+  fullScreenSize: {
+    height: "100vh",
+    width: "100vw",
+  } as CSSProperties,
+};
